d73d24b77928a1cba9297defedd9da216e2190af
codeforkjeff <jeff@codefork.com>
2018-12-04 22:47:38 -0800

    18x faster using stack

--- src/main/scala/org/codefork/aoc2018/Day5.scala
+++ src/main/scala/org/codefork/aoc2018/Day5.scala
@@ -9,7 +9,7 @@ object Day5 {
     Source.fromURL(url).mkString.stripLineEnd
   }
 
-  def reactPolymer(s: String, pos: Int = 0): String = {
+  def reactPolymerSlow(s: String, pos: Int = 0): String = {
     if (pos == s.length - 1) {
       s
     } else {
@@ -20,10 +20,23 @@ object Day5 {
         // this is way faster than patch()
         val reduced = s.substring(0, pos) + s.substring(pos + 2)
         val newPos = if (pos - 1 < 0) 0 else pos - 1
-        reactPolymer(reduced, newPos)
+        reactPolymerSlow(reduced, newPos)
       } else {
-        reactPolymer(s, pos + 1)
+        reactPolymerSlow(s, pos + 1)
       }
+    }
+  }
+
+  // slow version = ~1100ms for part 1 input; this version using list as stack takes ~60ms
+  def reactPolymer(s: String, reacted: List[Char] = List[Char](), pos: Int = 0): String = {
+    if (pos == s.length) {
+      reacted.mkString.reverse
+    } else {
+      val first = if(reacted.nonEmpty) reacted(0) else ' '
+      val second = s(pos)
+      val unitsReacted = first != second && first.toUpper == second.toUpper
+      val newReacted = if(unitsReacted) reacted.tail else second :: reacted
+      reactPolymer(s, newReacted, pos + 1)
     }
   }
 
