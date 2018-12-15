ab014804c29ba7c3ed6b1f01d3ede2d986c105c4
codeforkjeff <jeff@codefork.com>
2018-12-14 19:28:39 -0800

    day 11 part 2 - horrible brute force solution that takes forever

--- src/main/scala/org/codefork/aoc2018/Day11.scala
+++ src/main/scala/org/codefork/aoc2018/Day11.scala
@@ -2,26 +2,41 @@ package org.codefork.aoc2018
 
 object Day11 {
 
-  def createGrid(width: Int, height: Int) =
-    1.to(height).foldLeft(Map[(Int, Int), Int]()) { (acc, y) =>
-      1.to(width).foldLeft(acc) { (acc, x) =>
+  val INPUT = 7857
+
+  case class Grid(width: Int, height: Int, serialNumber: Int) {
+
+    val grid =
+      1.to(height).foldLeft(Map[(Int, Int), Int]()) { (acc, y) =>
+        1.to(width).foldLeft(acc) { (acc, x) =>
         {
           acc + ((x, y) -> getPower(x, y))
         }
+        }
       }
+
+    // possible values are -5 through 4
+    def getPower(x: Int, y: Int) = {
+      val rackId = x + 10
+      val i = ((rackId * y) + serialNumber) * rackId
+      ((i / 100) % 10) - 5
     }
 
-  // possible values are -5 through 4
-  def getPower(x: Int, y: Int) = {
-    val rackId = x + 10
-    val i = ((rackId * y) + 7857) * rackId
-    ((i / 100) % 10) - 5
+    def getPowerOfSquare(x: Int, y: Int, squareSize: Int) =
+      y.to(y + squareSize - 1).map(y_ => x.to(x + squareSize - 1).map(x_ => grid(x_, y_)).sum).sum
+
+    def getPower3x3(x: Int, y: Int) =
+      getPowerOfSquare(x, y, 3)
+
+    def display() = {
+      1.to(height).foreach(y => {
+        1.to(width).foreach(x =>{
+          print(grid(x, y).toString.reverse.padTo(3, ' ').reverse + " ")
+        })
+        println
+      })
+    }
+
   }
-
-  def getPowerOfSquare(grid: Map[(Int, Int), Int], x: Int, y: Int, squareSize: Int) =
-    y.to(y + squareSize - 1).map(y_ => x.to(x + squareSize - 1).map(x_ => grid(x_, y_)).sum).sum
-
-  def getPower3x3(grid: Map[(Int, Int), Int], x: Int, y: Int) =
-    getPowerOfSquare(grid, x, y, 3)
 
 }
--- src/main/scala/org/codefork/aoc2018/Day11Part1.scala
+++ src/main/scala/org/codefork/aoc2018/Day11Part1.scala
@@ -5,11 +5,11 @@ object Day11Part1 extends Part {
   override def answer: String = {
     val width = 300
     val height = 300
-    val grid = Day11.createGrid(width, height)
+    val grid = Day11.Grid(width, height, Day11.INPUT)
     val squares = 1.to(height - 2).foldLeft(Map[(Int, Int), Int]()) {
       (acc, y) =>
         1.to(width - 2).foldLeft(acc) { (acc, x) =>
-          acc + ((x, y) -> Day11.getPower3x3(grid, x, y))
+          acc + ((x, y) -> grid.getPower3x3(x, y))
         }
     }
     squares.maxBy(_._2)._1.toString
